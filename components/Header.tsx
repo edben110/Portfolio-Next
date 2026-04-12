0973739ffdcafde16e38452b5b5e3c14c72981e7
edben110 <edben110@gmail.com>
2026-04-12 17:40:53 -0500

    fix: logo rounded

--- components/Header.tsx
+++ components/Header.tsx
@@ -95,12 +95,13 @@ export default function Header() {
                 className="flex items-center gap-3 cursor-pointer"
                 onClick={() => scrollToSection('inicio')}
               >
-                <div className="relative w-12 h-12">
+                <div className="relative w-12 h-12 rounded-full overflow-hidden bg-black p-1">
                   <Image
                     src="/imgs/logo.jpeg"
                     alt="Logo Edben"
-                    fill
-                    className="object-contain rounded-full"
+                    width={40}
+                    height={40}
+                    className="w-full h-full object-contain"
                     priority
                   />
                 </div>
